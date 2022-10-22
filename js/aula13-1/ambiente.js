7112ae399be5445c435b100dc5897ec475e58795
alramosf <106263609+alramosf@users.noreply.github.com>
2022-10-22 15:20:40 -0300

    comandos de repetições JS

---
+++ js/aula13-1/ambiente.js
@@ -0,0 +1,29 @@
+
+var c = 1
+do{
+    console.log(`A contagem está em ${c}`)
+    c++
+} while (c <= 6)
+
+
+/*var c = 1
+while(c <= 500) {
+console.log(`A contagem está em ${c}`)
+c++ //c = c + 1
+}
+*/
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
+
